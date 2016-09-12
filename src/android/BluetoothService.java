f5548ede58f2e4f02b56eff7e75823138c3e71d1
martin <martin@jos-services.com>
2016-09-12 15:19:28 +0200

    Testing independent thread run3

--- src/android/BluetoothService.java
+++ src/android/BluetoothService.java
@@ -1,4 +1,4 @@
-package package com.josservices.cordova.plugins.bluetooth;
+package com.josservices.cordova.plugins.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
