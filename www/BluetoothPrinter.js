31d16554e469f8860aca066867a322c9bb37d33a
martin <martin@jos-services.com>
2016-09-14 20:03:44 +0200

    Bluetooth connection - initial5

--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -14,7 +14,7 @@ var BTPrinter = {
       exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
    },
    printBMP: function(fnSuccess, fnError, printernName, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printBMP", { "name" : printernName, "args": [str]});
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printBMP", [{ "name" : printernName, "args": [str]}]);
    },
    printText: function(fnSuccess, fnError, str){
       exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
